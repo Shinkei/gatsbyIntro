935a80c680ba3d73a6ec264ad7a2502e4d9b7204
Jorge Ramirez <j.ramirez-hernandez@sensory-minds.com>
2021-01-05 23:07:26 +0100

    add helment and graphql to get the static data and improve the metadata
    add helment and some sitemetadata to improve the seo and page metadata,
    this is called with a hook that uses the graphql query

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
 module.exports = {
-    plugins: ['gatsby-plugin-emotion']
-}
+  siteMetadata: {
+    title: 'Shinkei Gatsby',
+    description: 'shinkei gatsby site',
+  },
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+};
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Global, css } from '@emotion/core';
+import Helmet from 'react-helmet';
 import Header from './header';
+import useSiteMetadata from '../hooks/use-sitemetadata';
 
 const Layout = ({ children }) => {
+  const { title, description } = useSiteMetadata();
   return (
     <>
       <Global
@@ -46,6 +49,11 @@ const Layout = ({ children }) => {
           }
         `}
       />
+      <Helmet>
+        <html lang="en" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Helmet>
       <Header />
       <main
         css={css`
